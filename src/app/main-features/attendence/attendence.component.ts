d4da2a2f5acf880dddfee0aed10ec2d476b449e1
Misheck Lukhama <misheck.lukhama001@gmail.com>
2023-12-02 20:12:15 +0000

    added features

---
+++ src/app/main-features/attendence/attendence.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-attendence',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './attendence.component.html',
+  styleUrl: './attendence.component.scss'
+})
+export class AttendenceComponent {
+
+}
